97621c1e516e1e8b26dc43b4455640658d277edf
Sergei <@email.com>
2026-07-17 12:08:09 +0500

    feat: add PaymentViewSet

--- lms/views.py
+++ lms/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets, generics
-from .models import Course, Lesson
-from .serializers import CourseSerializer, LessonSerializer
+from rest_framework import viewsets, generics, permissions
+from .models import Course, Lesson, Payment
+from .serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -20,3 +20,22 @@ class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     """Получение, обновление и удаление одного урока"""
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    """ViewSet для управления платежами"""
+
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """Пользователь видит только свои платежи"""
+        user = self.request.user
+        if user.is_superuser:
+            return Payment.objects.all()
+        return Payment.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        """При создании платежа автоматически подставляется текущий пользователь"""
+        serializer.save(user=self.request.user)
