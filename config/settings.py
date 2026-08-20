ab718b5599b914163c75ce9dd5c78138ad34c781
Sergei <@email.com>
2026-08-20 17:02:59 +0500

    Fix: добавлен домен Railway в ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-dev')
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'drfhomework-production.up.railway.app']
 
 
 INSTALLED_APPS = [
